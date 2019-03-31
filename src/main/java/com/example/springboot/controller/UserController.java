5c1722be0309cfc2d06b44034fba08f0e26fbd5f
knekri <knehr.krisztian@gmail.com>
2019-03-31 16:00:10 +0200

    add controller class

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -2,23 +2,26 @@ package com.example.springboot.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.entities.User;
 import com.example.springboot.service.ServiceClass;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+
 @RestController
-@RequestMapping({"/api"})
 public class UserController {
 
 	ServiceClass userService;
@@ -28,29 +31,30 @@ public class UserController {
 		this.userService = service;
 	}
 	
-    @PostMapping
-    public User create(@RequestBody User user){
-        return userService.create(user);
-    }
+	@RequestMapping(path="/users", method = RequestMethod.GET)
+	public ResponseEntity<List<User>>  listUser(){
+		return new ResponseEntity<List<User>>(userService.findAll(), HttpStatus.OK);
+	}
 
-    @GetMapping(path = {"/{id}"})
-    public User findOne(@PathVariable("id") int id){
-        return userService.findById(id).get();
-    }
+	@RequestMapping(path="/user/{id}", method = RequestMethod.GET)
+	public ResponseEntity<User>  listUserById(@PathVariable(value = "id") String id){
+		User userById = userService.findById(Integer.valueOf(id)).get();
+		return new ResponseEntity<User>(userById, HttpStatus.OK);
+	}
 
-    @PutMapping
-    public User update(@RequestBody User user){
-        return userService.update(user);
-    }
-
-    @DeleteMapping(path ={"/{id}"})
-    public User delete(@PathVariable("id") int id) {
-        return userService.delete(id);
-    }
-
-    @GetMapping
-    public List<User> findAll(){
-        return userService.findAll();
-    }
+	@RequestMapping(path="/user", method = RequestMethod.POST)
+	public ResponseEntity<User>  listUser(@RequestBody User user){
+		return new ResponseEntity<User>(userService.create(user), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/logmeout", method = RequestMethod.POST)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		
+		return "redirect:/login";
+	}
 	
 }
