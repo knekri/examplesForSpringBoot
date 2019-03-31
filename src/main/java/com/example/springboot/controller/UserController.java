daf31faaf0cc3b3789683e702bd0b7b92e027877
knekri <knehr.krisztian@gmail.com>
2019-03-31 10:57:55 +0200

    add controller

---
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.example.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.entities.User;
+import com.example.springboot.service.ServiceClass;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
+@RequestMapping({"/api"})
+public class UserController {
+
+	ServiceClass userService;
+
+	@Autowired
+	public UserController(ServiceClass service) {
+		this.userService = service;
+	}
+	
+    @PostMapping
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public User findOne(@PathVariable("id") int id){
+        return userService.findById(id).get();
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+
+    @DeleteMapping(path ={"/{id}"})
+    public User delete(@PathVariable("id") int id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+	
+}
