ecb408bd3e4ebd46d7a807bcdf92d8bb7762f6da
knekri <knehr.krisztian@gmail.com>
2019-03-31 10:49:05 +0200

    add main classes
    (cherry picked from commit 647e270ee54160ab7070fded13f32c41e42b36d7)

---
+++ src/main/java/com/example/springboot/ExampleSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExampleSpringBootApplication.class, args);
+  }
+}
